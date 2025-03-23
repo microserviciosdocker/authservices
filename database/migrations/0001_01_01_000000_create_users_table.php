66d5b9a816e40913bc5bcd419cfbfb4a64531b5b
microserviciosdocker <microserviciosdocker@gmail.com>
2025-03-23 13:47:24 -0700

    Integrando sail al proyecto y configurando .env con seeders

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name',50);
+            $table->string('lastname',50);
+            $table->string('username',50)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('code',3)->nullable();
+            $table->string('phone',10)->nullable();
+            $table->string('chat_id',30)->nullable();
+            $table->string('profile_image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
